2ab0bcdb1a867ef617675539bda974a390e475e0
Rasster <rasimdogan3850@gmail.com>
2024-06-02 19:13:31 +0300

    Mvc İle Otel Rezevasyon Sitesi
    Solid ve patternler dikkate alınarak yapılmaya çalışılmıştır

---
+++ OtelMvcProje/OtelMvcProje/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OtelMvcProje.Controllers
+{
+    //Bu Sayfa Projenin kendisiyle birlikte gelmektedir bir işlem yapılmamıştır yapılar her bir işlem farklı controller yardımıyla yapılmıştır bilginize.
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ OtelMvcProje/OtelMvcProje/Controllers/RezervasyonController.cs
@@ -0,0 +1,35 @@
+﻿using OtelMvcProje.Models.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OtelMvcProje.Controllers
+{
+    public class RezervasyonController : Controller
+    {
+        // GET: Rezervasyon
+        DbOtelYeniEntitiess db = new DbOtelYeniEntitiess();
+
+        [Authorize]
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(TblOnRezervasyon t)
+        {
+            var misafirmail = (string)Session["Mail"];
+            // var misafirid = db.TblYeniKayit.Where(x => x.Mail == misafirmail).Select(x => x.ID).FirstOrDefault();
+            //p.Durum = 15;
+            //p.Misafir = misafirid; deneme verileri girilmiştir veritabanı sıfırlanıp tekrardan entegre edilmiştir idler 1den başlıyacaktır
+            t.Mail = misafirmail;
+            t.Tarih = DateTime.Parse(DateTime.Now.ToShortDateString());
+            db.TblOnRezervasyon.Add(t);
+            db.SaveChanges();
+            return RedirectToAction("Rezervasyonlarim","Misafir");
+        }
+    }
+}
